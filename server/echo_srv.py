58d9a28d33809be94ae6e69a3ff0ebed9024688b
Anton <over.uran@gmail.com>
2020-10-03 18:25:46 +0300

    echo_server

--- server/echo_srv.py
+++ server/echo_srv.py
@@ -1,13 +1,38 @@
 import socket
+
 server = True
 IP = input()
 port = int(input())
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((IP, port))
+sock.listen(5)
+
+print("Server is running")
+
 while server:
-    result = sock.recv(1024)
-    print('Сonnection', result.decode('utf-8'))
-    print(str(result))
-    if str(result) == "b'quit'":
-        server = False
-sock.close()
+    try:
+        client, addr = sock.accept()
+    except KeyboardInterrupt:
+        sock.close()
+        break
+    else:
+        client.send(b"Welcome")
+        print("Connection with ", client, addr)
+        while server:
+            try:
+                result = client.recv(1024)
+            except ConnectionResetError:
+                print("Connection with ", client, addr, " lost")
+                break
+            else:
+                print('Incoming message', result.decode('utf-8'))
+                ans = input()
+                ans = bytes(ans, 'utf-8')
+                client.send(ans)
+
+
+#         print(str(result))
+#         if str(result) == "b'quit'":
+#             server = False
+# sock.close()
