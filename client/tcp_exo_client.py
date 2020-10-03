088f010b127d9c5c950743c3604f16e89a7ec29e
Vova Vlasuk <vlasuk.roma04@gmail.com>
2020-10-03 18:31:15 +0300

    roma

---
+++ client/tcp_exo_client.py
@@ -0,0 +1,15 @@
+import socket
+
+IP = input('')
+port = input('')
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((IP, port))
+run = True
+while run:
+    result = sock.recv(1024)
+    print(result)
+    mess = input('')
+    mess = bytes(mess, 'utf-8')
+    sock.send(mess)
+sock.close()
