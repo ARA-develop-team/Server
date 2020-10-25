882a3cc7a834c17118bc82c01280dd1eb2272ed5
Anton <over.uran@gmail.com>
2020-10-25 19:35:03 +0200

    correction_of_formula

--- orbit/orbit1.py
+++ orbit/orbit1.py
@@ -13,22 +13,32 @@ class Object:
         self.radius = radius
         self.weight = weight
         self.impulse = impulse
+        self.G = 6.7 * (10 ** -11)  # G - gravitational constant
 
     def draw(self):
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius, self.radius)
 
+    def force_of_attraction(self, stranger_weight, distance):
+        F = self.G * ((stranger_weight * self.weight) / (distance ** 2))  # F - force of attraction
+        return F
+
+    def motion(self):
+        pass
+
 
 """test code"""
+distance = 55.76 * (10**6)
 planets = []
-for a in range(0, 5):
+for a in range(0, 2):
     x = random.randint(5, 455)
     y = random.randint(5, 455)
     color = (255, 255, 255)
     radius = random.randint(5, 20)
     impulse = 0
-    weight = 0
+    weight = 5.9726 * (10**24)
     planets.append(Object(x, y, color, radius, weight, impulse))
 # end of test code
+
 
 pygame.init()
 while run:
@@ -39,6 +49,12 @@ while run:
     window.fill((10, 10, 10))
 
     for planet in planets:
+        # test code
+        for second_planet in planets:
+            if planet != second_planet:
+                F = planet.force_of_attraction(second_planet.weight, distance)
+                print(int(F))
+        # end of test code
         planet.draw()
 
     pygame.display.update()
