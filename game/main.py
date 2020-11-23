6834184d5b2fd4f5fea270023feebe1871d8ef6d
Anton <over.uran@gmail.com>
2020-11-23 17:05:34 +0200

    game_class

--- game/main.py
+++ game/main.py
@@ -1,1 +1,12 @@
-print("Hello Roma!!!")
+
+
+class CPlayer:
+    def __init__(self, p_name, p_x, p_y):
+        self.name = p_name
+        self.x = p_x
+        self.y = p_y
+        self.speed = 10
+        self.size = 10
+        self.color = (255, 168, 18)
+        
+
