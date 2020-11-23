c785f2f6b7760f0eeb09a9da5bfcc627447cc2fc
IcePhoenix09 <63466963+IcePhoenix09@users.noreply.github.com>
2020-11-23 17:26:19 +0200

    rom

--- game/main.py
+++ game/main.py
@@ -6,6 +6,20 @@ screen_x = 500
 screen_y = 500
 window = pygame.display.set_mode((screen_x, screen_y))
 
+
+class CPlayer:
+    def __init__(self, p_name, p_x, p_y, p_color):
+        self.name = p_name
+        self.x = p_x
+        self.y = p_y
+        self.color = p_color
+        self.speed = 10
+        self.size = 10
+
+    def draw(self):
+        pygame.draw.circle(window, self.color, [self.x, self.y], self.size, self.size)
+
+
 run = True
 while run:
     for e in pygame.event.get():
@@ -16,13 +30,3 @@ while run:
 
     pygame.display.update()
 pygame.quit()
-
-class CPlayer:
-    def __init__(self, p_name, p_x, p_y):
-        self.name = p_name
-        self.x = p_x
-        self.y = p_y
-        self.speed = 10
-        self.size = 10
-        self.color = (255, 168, 18)
-
