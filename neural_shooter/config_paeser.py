d6eaca62d2793937943a1c0913e5c4e9a4b7668d
Anton <over.uran@gmail.com>
2021-02-05 16:50:59 +0200

    config_parser

---
+++ neural_shooter/config_paeser.py
@@ -0,0 +1,33 @@
+"""get IP and port for the neural shooter"""
+
+import yaml
+
+
+def getting_data(file_name):
+    with open(file_name) as file:
+        data = {}
+        data_yml = yaml.load(file, yaml.Loader)
+
+        try:
+            data['PORT'] = int(data_yml[1]['PORT'])
+        except ValueError:
+            print(f"[ValueError]  PORT must consist only of numbers! \nMake changes in the file '{file_name}'")
+            quit()
+
+        if data_yml[0]['IP'].lower() == 'default':
+            import socket
+            data['IP'] = socket.gethostbyname(socket.gethostname())
+
+        else:
+            data['IP'] = data_yml[0]['IP']
+
+        return data
+
+
+if __name__ == "__main__":
+    using_file = [r"client.yml", r"server.yml"]      # temp
+    for path in using_file:
+        result = getting_data(path)
+        print(result)
+
+
