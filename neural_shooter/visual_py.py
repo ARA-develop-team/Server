c38dad1b8919f77ffb6a3c74593889d7a2213e1b
Anton <over.uran@gmail.com>
2021-02-05 22:34:26 +0200

    with parsers

--- neural_shooter/visual_py.py
+++ neural_shooter/visual_py.py
@@ -6,5 +6,6 @@ window = pygame.display.set_mode((500, 500))
 def draw_screen(list_obj):
     window.fill((0, 100, 200))
     for obj in list_obj:
-        pygame.draw.circle(window, (0, 0, 0), obj, 10, 10)
+        if len(obj) != 0:
+            pygame.draw.circle(window, (0, 0, 0), obj, 10, 10)
     pygame.display.update()
