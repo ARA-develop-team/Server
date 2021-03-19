a3bfd977cdb87dd066851b5edab9d992eb1aad98
Vova Vlasuk <vlasuk.roma04@gmail.com>
2021-03-19 14:13:08 +0200

    server and client

--- neural_shooter/client_f.py
+++ neural_shooter/client_f.py
@@ -32,7 +32,6 @@ class Client():
         ans = pickle.loads(ans)
         return ans
 
-
     def data_exchange(self, msg):
         # send length
         message = pickle.dumps(msg)  # packing message
