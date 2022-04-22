b4c523c6a43d59c3f243e5ca0401222a32806ab7
ANTON <over.uran@gmail.com>
2022-04-22 11:07:43 +0200

    Editing start.yml

--- client/tcp_exo_client.py
+++ client/tcp_exo_client.py
@@ -1,18 +1,28 @@
 import socket
 
-IP = '176.38.153.161'
-port = 8585
+IP = '147.175.115.34'
+port = 777
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP, port))
 
-result = sock.recv(1024)
-print(result)
-
-number = 0
 run = True
+
 while run:
-    number += 1
-    mess = bytes(str(number), 'utf-8')
-    sock.send(mess)
+    result = sock.recv(1024)
+    print(result)
+
+    answer = input()
+    if answer == '%%':
+        break
+    # answer = bytes(answer, 'utf-8')
+    sock.send(answer)
+
+
 sock.close()
+
+# while run:
+#     number += 1
+#     mess = bytes(str(number), 'utf-8')
+#     sock.send(mess)
+# sock.close()
