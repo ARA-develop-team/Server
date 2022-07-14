7705ae52f81ab0f6bb5e33a6cdf4bd9e63316d60
Vova Vlasuk <vlasuk.roma04@gmail.com>
2022-07-14 11:55:42 +0300

    replace input_data to shooter_ui

--- shooter.py
+++ shooter.py
@@ -57,12 +57,12 @@ class Shooter:
             self.playing()
 
     def playing(self):
-        while self.run:
+        while self.user_visual.run:
             # print fps
             self.clock.tick(30)
             pygame.display.set_caption(f"FPS: {self.clock.get_fps()}")
 
-            event = self.input_data(self.field.player_dict[self.player_name])
+            event = self.user_visual.input_data(self.field.player_dict[self.player_name])
             self.field.event_process(event)
 
             self.field.player_collision_processing(self.player_name)
@@ -78,7 +78,7 @@ class Shooter:
 
         self.analysis.launch()
 
-        while self.run:
+        while self.user_visual.run:
             # print fps
             self.clock.tick(30)
             pygame.display.set_caption(f"FPS: {self.clock.get_fps()}")
@@ -86,12 +86,14 @@ class Shooter:
             # receive data_package from server
             self.field.player_dict, self.field.block_list, self.field.bullet_list = self.client.receive()
 
-            event = self.input_data(self.field.player_dict[self.player_name])
+            event = self.user_visual.input_data(self.field.player_dict[self.player_name])
             self.client.send(event)
 
             self.user_visual.draw_screen(self.field.player_dict, self.field.bullet_list,
                                          self.field.block_list, self.player_name)
             self.analysis.processing()
+
+        exit()
 
     def exit(self):  # cancel threads
         if self.user_visual is not None:
@@ -103,47 +105,6 @@ class Shooter:
 
         print("-----------END-----------")
         quit()
-
-    def input_data(self, player):
-        # TODO - ask about moving input_data to shooter.
-        # TODO - clean this peace of code.
-
-        player_way_vector = player.way_vector
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                self.run = False
-                self.exit()
-                break
-
-            if e.type == pygame.MOUSEMOTION:
-                self.mouse_pos = pygame.mouse.get_pos()
-                self.user_visual.mouse_pos = self.mouse_pos
-
-            if e.type == pygame.MOUSEBUTTONUP:
-                return [1, player.name]     # event shoot
-
-        keys = pygame.key.get_pressed()
-
-        player_pos_diff = [0, 0]
-        if keys[pygame.K_a]:
-            player_pos_diff[0] -= player.speed
-            player.pos[0] -= player.speed
-
-        if keys[pygame.K_d]:
-            player_pos_diff[0] += player.speed
-            player.pos[0] += player.speed
-
-        if keys[pygame.K_w]:
-            player_pos_diff[1] -= player.speed
-            player.pos[1] -= player.speed
-
-        if keys[pygame.K_s]:
-            player_pos_diff[1] += player.speed
-            player.pos[1] += player.speed
-
-        player_way_vector = [self.mouse_pos[0] - player.pos[0], self.mouse_pos[1] - player.pos[1]]
-        # event move
-        return [0, player.name, player_pos_diff, player_way_vector]
 
 
 if __name__ == "__main__":
--- ui/shooter_ui.py
+++ ui/shooter_ui.py
@@ -12,6 +12,7 @@ class CPygame:
         self.work_info = pygame.font.SysFont('monospace', 16)
         self.screen_color = screen_color
         self.mouse_pos = None
+        self.run = True
 
     def draw_screen(self, player_dict, bullet_list, block_list, player_name):
         self.window.fill(self.screen_color)
@@ -37,6 +38,43 @@ class CPygame:
 
         player_dict[player_name].draw_lines(self.mouse_pos, self.window, self.work_info)     # for lines
         pygame.display.update()
+
+    def input_data(self, player):
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                self.run = False
+                break
+
+            if e.type == pygame.MOUSEMOTION:
+                self.mouse_pos = pygame.mouse.get_pos()
+                self.mouse_pos = self.mouse_pos
+
+            if e.type == pygame.MOUSEBUTTONUP:
+                return [1, player.name]     # event shoot
+
+        keys = pygame.key.get_pressed()
+
+        player_pos_diff = [0, 0]
+        if keys[pygame.K_a]:
+            player_pos_diff[0] -= player.speed
+            player.pos[0] -= player.speed
+
+        if keys[pygame.K_d]:
+            player_pos_diff[0] += player.speed
+            player.pos[0] += player.speed
+
+        if keys[pygame.K_w]:
+            player_pos_diff[1] -= player.speed
+            player.pos[1] -= player.speed
+
+        if keys[pygame.K_s]:
+            player_pos_diff[1] += player.speed
+            player.pos[1] += player.speed
+
+        player_way_vector = [self.mouse_pos[0] - player.pos[0], self.mouse_pos[1] - player.pos[1]]
+
+        # event move
+        return [0, player.name, player_pos_diff, player_way_vector]
 
 
 def distance_between_two_point(a, b):
